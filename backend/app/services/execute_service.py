106e38a4edabdceeec9ef20401a27990fac16d2f
Mariano Martin <marianomartin@Marianos-MacBook-Pro.local>
2021-03-24 21:34:14 -0300

    add formula regex validation in backend

--- backend/app/services/execute_service.py
+++ backend/app/services/execute_service.py
@@ -1,8 +1,7 @@
 import re
 
 def validate_formula(formula):
-    # Here we validate invalid characters using re 
-    return True
+    return bool(re.match('^[0-9,+,(,),*,-,/]*$', formula))
 
 def execute_formula(formula):
     return eval(formula)
